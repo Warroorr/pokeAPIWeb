810db3bbdd15fd99fbe5e837359eaa0152dd2123
Alex Campillo <alexcampillo1@gmail.com>
2025-08-06 14:04:52 +0200

    Configuración inicial del proyecto: - Configuración de nuxt 3 - Configuración de tailwindcss y postcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        './components/**/*.{vue,js,ts}',
+        './layouts/**/*.{vue,js,ts}',
+        './pages/**/*.{vue,js,ts}',
+        './app.vue',
+        './plugins/**/*.{js,ts}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
