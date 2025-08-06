9ae117e598d8e309421b8c8a587f087b10c631a5
Alex Campillo <alexcampillo1@gmail.com>
2025-08-06 20:40:19 +0200

    Cambiado estilo del header

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-  <header class="bg-red-600 text-white py-4 text-center shadow-md">
-    <h1 class="text-3xl font-bold">Listado de Pokémon</h1>
+  <header class="bg-gradient-to-r from-red-600 to-pink-600 text-white shadow-lg py-6">
+    <div class="container mx-auto flex items-center justify-center gap-4">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" alt="Pokéball" class="w-10 h-10 animate-bounce" />
+      <h1 class="text-4xl font-bold tracking-wide drop-shadow-lg">
+        PokéApp
+      </h1>
+    </div>
   </header>
 </template>
